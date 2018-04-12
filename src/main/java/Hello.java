2979912b6f4f1b58db0ec7f524b37104b2868cb5
name:MrWang6688 <email@example.com:908569371@qq.com>
2018-04-12 17:15:34 +0800

    hello.java 的一次修改

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018\4\12 0012.
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("this is a test git 17:14");
+    }
+}
