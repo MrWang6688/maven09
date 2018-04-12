03437d14b1b627820e9191ac080229d4bfa38847
name:MrWang6688 <email@example.com:908569371@qq.com>
2018-04-12 18:04:54 +0800

    修改后的

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("this is a test git 17:14");
         System.out.print("hello ---by test");
+        System.out.print("修改后的hello");
     }
 }
