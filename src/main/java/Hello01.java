90a10b3fe5911c24ba4925ce9532097164040ea7
name:MrWang6688 <email@example.com:908569371@qq.com>
2018-04-12 17:20:46 +0800

    添加了hello01

---
+++ src/main/java/Hello01.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018\4\12 0012.
+ */
+public class Hello01 {
+    public static void main(String args[]){
+        System.out.print("this is a test 17:18");
+    }
+}
